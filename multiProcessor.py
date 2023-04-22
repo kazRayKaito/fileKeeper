687203c4f9377bb54d0152d0ed8ceffe46fb65ea
Kazuki Kaito <kaz.ray.kaito@gmail.com>
2023-04-23 08:48:46 +0900

    Added multiProcessor

---
+++ multiProcessor.py
@@ -0,0 +1,35 @@
+import os
+import multiprocessing
+import dirRenamer
+from datetime import datetime as dt
+
+todaysdate = dt.now()
+todaysdateStamp = todaysdate.strftime("%Y-%m-%d")
+
+rootDir = "/Volumes/raySSD2T/Data/rootFolder"
+dirListPath = os.getcwd() + "/dirList.csv"
+
+print(dirListPath)
+
+#dirList.csvを開いて、1行ずつ読み込み
+f = open(dirListPath, 'r')
+dirListLines = f.readlines()
+
+for dirListLine in dirListLines:
+    #各行をカンマで分割し、変数代入
+    items = dirListLine.split(',')
+    rootPath = items[0]
+    folderStructure = items[1]
+    preservationDays = items[2]
+    monthlyArchiveNumber = items[3]
+    
+    dirRenamer.rename(rootPath, folderStructure, preservationDays, monthlyArchiveNumber)
+
+#for each dir do following
+
+# for folder structure  = "root-folder-file"
+# read all folders and sort by name oldest to newest
+# dont add to list if folder starts with "[_"
+# check all the files and if the newest is older than 14 days, rename to [_YYYY-MM-DD_HH-mm-ss_]_Original Folder Name
+
+# for folder structure = "root-file"
